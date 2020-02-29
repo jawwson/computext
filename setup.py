9738c39437be25f616fa8a99400ec945eea27a15
Jason is Jawwson <59033313+jawwson@users.noreply.github.com>
2020-02-29 11:26:16 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Jason "Jawwson" Wang',                   # Type in your name
   author_email = 'jawwson@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/jawwson/computype',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jawwson/computype/archive/v_0.0.1.tar.gz',    # I explain this later on
   keywords = ['TYPING', 'CLEAR', 'BACKSPACE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
